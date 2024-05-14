394fac15c7575043cd6a5b2cc5cbd96bff2bdef1
SirineBETTAIEB <sirinebettaieb30@gmail.com>
2024-05-14 11:33:14 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , MyComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-component.component.html',
+  styleUrl: './my-component.component.scss'
+})
+export class MyComponentComponent {
+
+  isAdmin: boolean = false;
+  toggelAdmin() {
+    this.isAdmin = !this.isAdmin;
+  }
+}
